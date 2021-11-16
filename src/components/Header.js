2c5c0b0a6a3a23f33dae8ab06b6707ef99fc38b7
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-16 13:16:22 -0800

    Finished landing page and items

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,15 @@ const Header = () =>{
     return(
     <HeaderStyle>
         <h1>African Marketplace</h1>
-        <NavStyle>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <h2>Empowering small business owners in South Africa</h2>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/dashboard">Dash</Link></li>
-        </NavStyle>
+        <NavContainer>
+            <NavStyle>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/signup">Signup</Link></li>
+                <h2>Empowering small business owners in South Africa</h2>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/dashboard">Dash</Link></li>
+            </NavStyle>
+        </NavContainer>
     </HeaderStyle>)
 }
 export default Header
@@ -24,12 +26,15 @@ const HeaderStyle = styled.div`
     align-items: center;
     justify-content: center;
     width:100%;
+    margin:0px;
+    padding:0px;
 
     h1{
         background-color:white;
         width:100%;
         font-size:4rem;
         margin:0;
+        text-align:center;
     }
 
     p {
@@ -37,13 +42,20 @@ const HeaderStyle = styled.div`
         font-size: 1.5em;
     }
 `
-
+const NavContainer=styled.div`
+    width:100%;
+    display:flex;
+    justify-content:center;
+    background-color: #fcd5ce;
+`
 const NavStyle = styled.ul`
     display:flex;
     justify-content:space-around;
     align-items:center;
     width:1000px;
-    height:20px;
+    height:40px;
+    margin:0px;
+    padding:0px;
   li {
         display: flex;
         flex-direction:row;
@@ -57,6 +69,9 @@ const NavStyle = styled.ul`
        
     }
     h2{
-            font-size:1rem;
+            font-size:1.2rem;
+            display:flex;
+            align-items:center;
+            justify-content:center;
         }
 `
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,133 @@
-//This is the landing page
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import LandingItem from './LandingItem'
+
+const initialItems = [
+    {
+        id: 1,
+        name: 'eggs',
+        description: 'white',
+        price: '$3',
+        location: 'US'
+    },
+    {   
+        id:2,
+        name: 'milk',
+        description: 'white',
+        price: '$4',
+        location: 'US'
+    },
+    {
+        id:3,
+        name: 'Pineapple Juice',
+        description: 'orange',
+        price: '$5',
+        location: 'Hawaii'
+    },
+    {
+        id:4,
+        name: 'Cheese',
+        description: 'white',
+        price: '$6',
+        location: 'Italy'
+    },
+    {
+        id: 5,
+        name: 'Chocolate',
+        description: 'brown',
+        price: '$2',
+        location: 'Germany'
+    },
+    {   
+        id:6,
+        name: 'Rice',
+        description: 'white',
+        price: '$4',
+        location: 'Spain'
+    },
+    {
+        id:7,
+        name: 'Wheat',
+        description: 'golden',
+        price: '$5',
+        location: 'US'
+    },
+    {
+        id:8,
+        name: 'Wine',
+        description: 'white',
+        price: '$10',
+        location: 'New Zealand'
+    }
+]
+
 
 const LandingPage = () =>{
-    return(
-    <div>I am the landing page</div>
-    )
-}
+    const[items, setItems]=useState(initialItems)
+    const [isToggled,setIsToggled]=useState(false)
 
-export default LandingPage
+
+    const toggle=()=>{
+        setIsToggled(!isToggled)
+        console.log(isToggled)
+    }
+
+    return(
+    <HomeStyle>
+        <h2>See what our users have to offer!</h2>
+        {isToggled && <ListStyle>
+            <LandingItem item={items[0]}/>
+            <LandingItem item={items[1]}/>
+            <LandingItem item={items[2]}/>
+            <LandingItem item={items[3]}/>
+        </ListStyle>}
+        {
+        !isToggled &&
+        <ListStyle>
+            {items.map(item=>{
+            return <LandingItem key={item.id} item={item}/>
+            })}
+        </ListStyle>
+        }
+        <p>Click <span onClick={toggle}>HERE</span> {isToggled ? 'to browse more items...' : 'to hide items...'}</p>
+        <Link to="/login" className='styledLink'>or click <span>HERE</span> to list your own! </Link>
+    </HomeStyle>
+    )}
+export default LandingPage;
+
+const HomeStyle = styled.ul`
+    display:flex;
+    flex-direction:column;
+    justify-content:space-around;
+    align-items:center;
+   
+  li {
+        display: flex;
+        flex-direction:row;
+        padding: 0.3rem 1rem;
+        
+        a {
+            text-decoration: none;
+            color: black;
+            font-size: 1.4em;
+        }
+    }
+    h2{
+            font-size:2rem;
+        }
+    span{
+        font-weight:bold;
+    }
+    span:hover{
+        cursor:pointer;
+    }
+    .styledLink{
+        text-decoration:none;
+        color:black;
+    }
+`
+const ListStyle = styled.ul`
+    display:flex;
+    flex-wrap:wrap;
+`
